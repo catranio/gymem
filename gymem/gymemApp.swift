9c28801cc286931a08a8c5b4cc692daaae44e057
Pitr Iaskevich <iaskdeveloper@gmail.com>
2024-10-20 22:03:03 +0300

    Initial Commit

---
+++ gymem/gymemApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymemApp.swift
+//  gymem
+//
+//  Created by Петр Яскевич on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct gymemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
